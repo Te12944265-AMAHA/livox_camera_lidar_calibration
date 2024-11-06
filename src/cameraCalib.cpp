11405a30a27e6aa38d53cee03b33bcd8c273e931
Te12944265-AMAHA <1603699457@qq.com>
2024-11-06 14:11:37 -0500

    (migrate changes from another deprecated repo): fix build error on 20.04

--- src/cameraCalib.cpp
+++ src/cameraCalib.cpp
@@ -23,126 +23,124 @@ void getParameters() {
     cout << "Get the parameters from the launch file" << endl;
 
     if (!ros::param::get("camera_in_path", camera_in_path)) {
-        cout << "Can not get the value of camera_in_path" << endl;
+        cout << "Cannot get the value of camera_in_path" << endl;
         exit(1);
     }
     if (!ros::param::get("camera_folder_path", camera_folder_path)) {
-        cout << "Can not get the value of camera_folder_path" << endl;
+        cout << "Cannot get the value of camera_folder_path" << endl;
         exit(1);
     }
     if (!ros::param::get("result_path", result_path)) {
-        cout << "Can not get the value of result_path" << endl;
+        cout << "Cannot get the value of result_path" << endl;
         exit(1);
     }
     if (!ros::param::get("row_number", row_number)) {
-        cout << "Can not get the value of row_number" << endl;
+        cout << "Cannot get the value of row_number" << endl;
         exit(1);
     }
     if (!ros::param::get("col_number", col_number)) {
-        cout << "Can not get the value of col_number" << endl;
+        cout << "Cannot get the value of col_number" << endl;
         exit(1);
     }
     if (!ros::param::get("width", width)) {
-        cout << "Can not get the value of width" << endl;
+        cout << "Cannot get the value of width" << endl;
         exit(1);
     }
     if (!ros::param::get("height", height)) {
-        cout << "Can not get the value of height" << endl;
+        cout << "Cannot get the value of height" << endl;
         exit(1);
     }
 
 	if (!ros::param::get("fisheye", isFisheye)) {
-        cout << "Can not get the value of fisheye" << endl;
+        cout << "Cannot get the value of fisheye" << endl;
         exit(1);
     }
 }
+
 
 int main(int argc, char **argv) {
 	ros::init(argc, argv, "cameraCalib");
 	getParameters();
 
-	ifstream fin(camera_in_path);   /* 标定所用图像文件的路径 */
-	ofstream fout(result_path);    /* 保存标定结果的文件 */
-	// 读取每一幅图像，从中提取出角点，然后对角点进行亚像素精确化
-	int image_count = 0;  /* 图像数量 */
-	Size image_size;      /* 图像的尺寸 */
-	Size board_size = Size(row_number, col_number);             /* 标定板上每行、列的角点数 */
-	vector<Point2f> image_points_buf;         /* 缓存每幅图像上检测到的角点 */
-	vector<vector<Point2f>> image_points_seq; /* 保存检测到的所有角点 */
-	string filename;      // 图片名
-	vector<string> filenames;
+	ifstream fin(camera_in_path);  
+	ofstream fout(result_path);   
+	int image_count = 0;  
+	Size image_size;     
+	Size board_size = Size(row_number, col_number); 
+	vector<Point2f> image_points_buf;        
+	vector<vector<Point2f>> image_points_seq; 
+	string filename; 
+	vector<string> filenames; // filenames of images whose corners can be found
+
+	// try finding checkerboard in all input images, if found append to filenames, else discard
 	while (getline(fin, filename) && filename.size() > 1) {
-		++image_count;
 		filename = camera_folder_path + filename;
 		cout << filename << endl;
 		Mat imageInput = imread(filename);
 		if (imageInput.empty()) {  // use the file name to search the photo
-        	break;
+			cout << "**" << filename << "** doesn't exist!\n";
+        	continue;
     	}
-		filenames.push_back(filename);
 
-		// 读入第一张图片时获取图片大小
-		if (image_count == 1) {
+		if (image_count == 0) {
 			image_size.width = imageInput.cols;
 			image_size.height = imageInput.rows;
 		}
 
-		/* 提取角点 */
+		// extract corners
 		if (0 == findChessboardCorners(imageInput, board_size, image_points_buf)) {
-			//cout << "can not find chessboard corners!\n";  // 找不到角点
-			cout << "**" << filename << "** can not find chessboard corners!\n";
-			exit(1);
+			cout << "**" << filename << "** cannot find checkerboard corners!\n";
+			continue;
 		}
 		else {
+			image_count++;
+			filenames.push_back(filename);
 			Mat view_gray;
-			cvtColor(imageInput, view_gray, cv::COLOR_RGB2GRAY);  // 转灰度图
+			cvtColor(imageInput, view_gray, cv::COLOR_RGB2GRAY);
 
-			/* 亚像素精确化 */
-			// image_points_buf 初始的角点坐标向量，同时作为亚像素坐标位置的输出
-			// Size(5,5) 搜索窗口大小
-			// （-1，-1）表示没有死区
-			// TermCriteria 角点的迭代过程的终止条件, 可以为迭代次数和角点精度两者的组合
+			// refine the corners to subpixel accuracy
 			cornerSubPix(view_gray, image_points_buf, Size(5, 5), Size(-1, -1), TermCriteria(cv::TermCriteria::EPS + cv::TermCriteria::MAX_ITER, 30, 0.1));
 
-			image_points_seq.push_back(image_points_buf);  // 保存亚像素角点
+			image_points_seq.push_back(image_points_buf); 
 
-			/* 在图像上显示角点位置 */
+			// display corners
 			Mat gray_RGB;
 			cvtColor(view_gray, gray_RGB, cv::COLOR_GRAY2RGB);
-			drawChessboardCorners(gray_RGB, board_size, image_points_buf, true); // 用于在图片中标记角点
+			drawChessboardCorners(gray_RGB, board_size, image_points_buf, true);
 			cv::resize(gray_RGB, gray_RGB, cv::Size(), 0.3, 0.3);
-			imshow("Camera Calibration", gray_RGB);       // 显示图片
+			imshow("Camera Calibration", gray_RGB); 
 
-			waitKey(500); //暂停1S      
+			waitKey(500);    
 		}
 	}
-	// int CornerNum = board_size.width * board_size.height;  // 每张图片上总的角点数
+
+	if (image_count == 0){
+		cout << "Not enough data!" << endl;
+		exit(1);
+	}
 	
-	//-------------以下是摄像机标定------------------
+	//-------------camera intrinsics calibration------------------
 
-	/*棋盘三维信息*/
-	Size square_size = Size(width, height);         /* 实际测量得到的标定板上每个棋盘格的大小 */
-	vector<vector<Point3f>> object_points;   /* 保存标定板上角点的三维坐标 */
+	Size square_size = Size(width, height);  
+	vector<vector<Point3f>> object_points; 
 
-	/*内外参数*/
-	Mat cameraMatrix = Mat(3, 3, CV_32FC1, Scalar::all(0));  /* 摄像机内参数矩阵 */
-	vector<int> point_counts;   // 每幅图像中角点的数量
-	Mat distCoeffs = Mat(1, 5, CV_32FC1, Scalar::all(0));       /* 摄像机的5个畸变系数：k1,k2,p1,p2,k3 */
-	Mat distCoeffs_fisheye = Mat(1, 4, CV_32FC1, Scalar::all(0));  /* θ_distorted = θ*(1+k1*θ^2+k2*θ^4+k3*θ^6+k4*θ^8) */
-	vector<Mat> tvecsMat;      /* 每幅图像的旋转向量 */
-	vector<Mat> rvecsMat;      /* 每幅图像的平移向量 */
+	Mat cameraMatrix = Mat(3, 3, CV_32FC1, Scalar::all(0));
+	vector<int> point_counts; 
+	Mat distCoeffs = Mat(1, 5, CV_32FC1, Scalar::all(0));       // k1,k2,p1,p2,k3
+	Mat distCoeffs_fisheye = Mat(1, 4, CV_32FC1, Scalar::all(0));  // θ_distorted = θ*(1+k1*θ^2+k2*θ^4+k3*θ^6+k4*θ^8) 
+	vector<Mat> tvecsMat;
+	vector<Mat> rvecsMat;
 
-	/* 初始化标定板上角点的三维坐标 */
 	int i, j, t;
 	for (t = 0; t<image_count; t++) {
 		vector<Point3f> tempPointSet;
 		for (i = 0; i<board_size.height; i++) {
 			for (j = 0; j<board_size.width; j++) {
 				Point3f realPoint;
-
-				/* 假设标定板放在世界坐标系中z=0的平面上 */
-				realPoint.x = i * square_size.width;
-				realPoint.y = j * square_size.height;
+				// in the checkerboard frame, x axis is parallel with board height, y is parallel with board width
+				// upper left corner is the origin
+				realPoint.x = i * square_size.height;
+				realPoint.y = j * square_size.width;
 				realPoint.z = 0;
 				tempPointSet.push_back(realPoint);
 			}
@@ -150,36 +148,24 @@ int main(int argc, char **argv) {
 		object_points.push_back(tempPointSet);
 	}
 
-	/* 初始化每幅图像中的角点数量，假定每幅图像中都可以看到完整的标定板 */
 	for (i = 0; i<image_count; i++) {
 		point_counts.push_back(board_size.width * board_size.height);
 	}
 
-	/* 开始标定 */
-	// object_points 世界坐标系中的角点的三维坐标
-	// image_points_seq 每一个内角点对应的图像坐标点
-	// image_size 图像的像素尺寸大小
-	// cameraMatrix 输出，内参矩阵
-	// distCoeffs 输出，畸变系数
-	// rvecsMat 输出，旋转向量
-	// tvecsMat 输出，位移向量
-	// 0 标定时所采用的算法
 	if (!isFisheye)
 		calibrateCamera(object_points, image_points_seq, image_size, cameraMatrix, distCoeffs, rvecsMat, tvecsMat, 0);
 	else
 		fisheye::calibrate(object_points, image_points_seq, image_size, cameraMatrix, distCoeffs_fisheye, rvecsMat, tvecsMat, fisheye::CALIB_RECOMPUTE_EXTRINSIC);
 
-	//------------------------标定完成------------------------------------
 
-	// -------------------对标定结果进行评价------------------------------
+	// -------------------evaluation------------------------------
 
-	double total_err = 0.0;         /* 所有图像的平均误差的总和 */
-	double err = 0.0;               /* 每幅图像的平均误差 */
-	vector<Point2f> image_points2;  /* 保存重新计算得到的投影点 */
+	double total_err = 0.0; 
+	double err = 0.0; 
+	vector<Point2f> image_points2; 
 	fout << "Average error: \n";
 	ifstream fin2(camera_in_path); 
 	for (i = 0; i < filenames.size(); i++){
-	//for (i = 0; i<image_count; i++) {
 		string fname = filenames[i];
 		Mat imageInput = imread(fname);
 		const char delim = '/';
@@ -197,13 +183,11 @@ int main(int argc, char **argv) {
 
 		vector<Point3f> tempPointSet = object_points[i];
 
-		/* 通过得到的摄像机内外参数，对空间的三维点进行重新投影计算，得到新的投影点 */
 		if (!isFisheye)
 			projectPoints(tempPointSet, rvecsMat[i], tvecsMat[i], cameraMatrix, distCoeffs, image_points2);
 		else
 			fisheye::projectPoints(tempPointSet, image_points2, rvecsMat[i], tvecsMat[i], cameraMatrix, distCoeffs_fisheye);
 
-		/* 计算新的投影点和旧的投影点之间的误差*/
 		vector<Point2f> tempImagePoint = image_points_seq[i];
 		Mat tempImagePointMat = Mat(1, tempImagePoint.size(), CV_32FC2);
 		Mat image_points2Mat = Mat(1, image_points2.size(), CV_32FC2);
@@ -214,7 +198,7 @@ int main(int argc, char **argv) {
 		}
 		err = norm(image_points2Mat, tempImagePointMat, NORM_L2);
 		total_err += err /= point_counts[i];
-		fout << "The error of picture " << i + 1 << " is " << err << " pixel" << endl;
+		fout << "The error of " << fname << " is " << err << " pixel" << endl;
 
 		if (!isFisheye)
 			undistort(view_gray, undistorted_gray, cameraMatrix, distCoeffs);
@@ -229,20 +213,25 @@ int main(int argc, char **argv) {
 	fout << "Overall average error is: " << total_err / image_count << " pixel" << endl << endl;
 	cout << "Overall average error is: " << total_err / image_count << " pixel" << endl << endl;
 
-	//-------------------------评价完成---------------------------------------------
 
-	//-----------------------保存定标结果------------------------------------------- 
-	Mat rotation_matrix = Mat(3, 3, CV_32FC1, Scalar::all(0));  /* 保存每幅图像的旋转矩阵 */
+	//-----------------------save data------------------------------------------- 
+	Mat rotation_matrix = Mat(3, 3, CV_32FC1, Scalar::all(0));
 	fout << "Intrinsic: " << endl;
 	fout << cameraMatrix << endl << endl;
+	cout << "Intrinsic: " << endl;
+	cout << cameraMatrix << endl << endl;
 	if (!isFisheye) {
 		fout << "Regular distortion parameters: " << endl;
 		fout << distCoeffs << endl << endl << endl;
+		cout << "Regular distortion parameters: " << endl;
+		cout << distCoeffs << endl << endl << endl;
 	} else {
 		fout << "Fisheye distortion parameters: " << endl;
 		fout << distCoeffs_fisheye << endl << endl << endl;
+		cout << "Fisheye distortion parameters: " << endl;
+		cout << distCoeffs_fisheye << endl << endl << endl;
 	}
-	cout << "Get result!" << endl;
+	cout << "Results saved." << endl;
 
 	fin.close();
 	fout.close();
